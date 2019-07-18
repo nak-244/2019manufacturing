f614c36881069ffdd4146a5566ad1ae82fffcac8
nak-244 <t09024656860@gmail.com>
2019-07-18 13:11:06 +0900

    up

---
+++ docs/js/search.js
@@ -0,0 +1,10 @@
+  <script type="text/javascript">
+    $(function() {
+      var href = "https://sigotora.jp/index.cfm";
+      $('#srh_ken_param,#sfw').on('change', function() {
+        var srh_ken_param = $('#srh_ken_param').val();
+        var sfw = $('#sfw').val();
+        $('#test').prop('href', href + "?fuseaction=job.joblist&srh_jobtype_param=10&srh_ken_param=" + srh_ken_param + "&sfw=" + sfw);
+      });
+    });
+  </script>
