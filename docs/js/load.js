aca0d0f798c46236708f1081b5ff56d4b03a844f
nak-244 <t09024656860@gmail.com>
2019-07-22 16:45:14 +0900

    up

--- docs/js/load.js
+++ docs/js/load.js
@@ -1,11 +1,11 @@
 $('head').append(
     '<style type="text/css">#container { display: none; } #fade, #loader { display: block; }</style>'
 );
- 
+
 jQuery.event.add(window,"load",function() { // 全ての読み込み完了後に呼ばれる関数
     var pageH = $("#container").height();
- 
-    $("#fade").css("height", pageH).delay(900).fadeOut(800);
-    $("#loader").delay(600).fadeOut(300);
+
+    $("#fade").css("height", pageH).delay(1000).fadeOut(800);
+    $("#loader").delay(600).fadeOut(900);
     $("#container").css("display", "block");
 });
