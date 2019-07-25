1bad102155994b4fb8e7a5e05a5356e6c2fcc5bd
nak-244 <t09024656860@gmail.com>
2019-07-25 13:49:55 +0900

    up

---
+++ docs/js/slick_option.js
@@ -0,0 +1,17 @@
+  infinite: true,
+  slidesToShow: 7,
+  slidesToScroll: 1,
+  arrows: false,
+  autoplay: true,
+  cssEase: 'linear',
+  autoplaySpeed: 0,
+  speed: 8000,
+  responsive: [
+   {
+    breakpoint: 768, //767px以下のサイズに適用
+    settings: {
+      slidesToShow:2,
+    }
+   }
+ ]
+});
